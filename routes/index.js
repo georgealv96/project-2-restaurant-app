9f71d19e758be74d4cecfe209400ce6dd7b5445d
georgealv96 <george1996alvarado@gmail.com>
2023-07-08 16:39:18 -0600

    work on navigation bar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 
 // The root route renders our only view
 router.get('/', function (req, res) {
-  res.redirect('/foodItems')
+  res.render('index')
 })
 
 // Google OAuth login route
@@ -19,8 +19,8 @@ router.get(
 router.get(
   '/oauth2callback',
   passport.authenticate('google', {
-    successRedirect: '/foodItems', // Where you want the client to go when login was successful
-    failureRedirect: '/foodItems' // Where you want the client to go if it fails
+    successRedirect: '/', // Where you want the client to go when login was successful
+    failureRedirect: '/' // Where you want the client to go if it fails
   })
 )
 
@@ -28,7 +28,7 @@ router.get(
 router.get('/logout', function (req, res) {
   req.logout(function () {
     //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/foodItems') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
