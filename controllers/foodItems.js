4a3faa95183669692f47e980f181d2eee03b0f12
georgealv96 <george1996alvarado@gmail.com>
2023-07-10 15:08:39 -0600

    show list of food items and each individual item's page

--- controllers/foodItems.js
+++ controllers/foodItems.js
@@ -1,11 +1,24 @@
 const FoodItem = require('../models/foodItem')
 
 module.exports = {
-  index
+  index,
+  show
 }
 
 async function index(req, res) {
+  // Grabbing every food item from the database
   const foodItems = await FoodItem.find({})
-  console.log(foodItems)
+  // Rendering and passing the food items array so it can be used in the views
   res.render('foodItems/index', { foodItems })
+}
+
+async function show(req, res) {
+  try {
+    // Grabbing and individual food item from the database
+    const foodItem = await FoodItem.findById(req.params.id)
+    // Rendering and passing the food item object so it can be used in the views
+    res.render('foodItems/show', { foodItem })
+  } catch (err) {
+    res.send(err)
+  }
 }
--- routes/foodItems.js
+++ routes/foodItems.js
@@ -5,5 +5,7 @@ const foodItemsCtrl = require('../controllers/foodItems')
 
 // GET /foodItems
 router.get('/', foodItemsCtrl.index)
+// GET /foodItems/:id
+router.get('/:id', foodItemsCtrl.show)
 
 module.exports = router
