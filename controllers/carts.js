427ae99a301947508e1918f8fc3a0f20fd309c6a
georgealv96 <george1996alvarado@gmail.com>
2023-07-12 11:24:45 -0600

    finishing up showing the items in the cart with their respective information and price

--- controllers/carts.js
+++ controllers/carts.js
@@ -12,20 +12,27 @@ async function index(req, res) {
       return res.render('cart/index', {
         errorMsg: "YOU HAVEN'T LOGGED IN YET!"
       })
-    // Find a cart that matches the user
+    // Finding a cart that matches the user
     const cart = await Cart.findOne({ user: req.user._id })
-    ////
-    // cart.foodItems.forEach(async function (i, index) {
-    //   const eachItem = await FoodItem.findById(i)
-    //   if (eachItem.addedToCart > 0) {
-    //     cart.foodItems.splice(index, 1)
-    //   }
-    //   eachItem.addedToCart++
-    //   await eachItem.save()
-    // })
-    // const itemsInTheCart = []
-    ////
-    res.render('cart/index', { cart })
+    // Creating an array with all the information we need to pass to the view
+    const itemsInCart = []
+    // Setting a variable that will hold the price of all of the items in the cart together
+    let totalAmount = 0
+
+    // Iterating throughout the list to search the information of each item in the cart
+    for (eachItem of cart.foodItems) {
+      // Adding to an array an object that contains the item information and its quantity found in the cart
+      itemsInCart.push({
+        item: await FoodItem.findById(eachItem.item),
+        amount: eachItem.amount
+      })
+      // Adding the price of each single item multiplied by its quantity
+      totalAmount +=
+        (await FoodItem.findById(eachItem.item)).price * eachItem.amount
+    }
+
+    // Rendering the cart/index view, passing some information
+    res.render('cart/index', { cart, itemsInCart, totalAmount })
   } catch (err) {
     res.send(err)
   }
